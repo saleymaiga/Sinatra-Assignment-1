64114f0c59f94257f53aab4d27f29b809971e602
saley Maiga <saleymaiga@gmail.com>
2014-02-12 22:22:10 -0500

    initial commit

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 
-get '/index' do 
-   erb :home
+get '/' do 
+   erb :index
+end
+
+get '/html' do
+   erb :html_page
+end
+
+get '/css' do
+   erb :css_page
+end
+
+get '/git_github' do
+   erb :git_github_page
 end
 
